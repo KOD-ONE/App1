2d37cc07f7a4df575571f2854b5005c895a363de
KOD-ONE <1046083856@qq.com>
2018-04-02 23:13:00 +0800

    简易视频播放器

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +30,41 @@ namespace App1
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        private async System.Threading.Tasks.Task openMediaFile()
+        {
+            FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".mp3");
+            fileOpenPicker.FileTypeFilter.Add(".mp4");
+
+            StorageFile file = await fileOpenPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                MediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
+                MediaPlayer.Visibility = Visibility.Visible;
+                Button.Visibility = Visibility.Collapsed;
+
+                MediaPlayer.MediaPlayer.MediaEnded += new TypedEventHandler<Windows.Media.Playback.MediaPlayer, object>(async (player, resource) => {
+                    await Button.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        Button.Visibility = Visibility.Visible;
+                        MediaPlayer.Visibility = Visibility.Collapsed;
+                    });
+                });
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            openMediaFile();
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button.Visibility =Visibility.Visible;
         }
     }
 }
